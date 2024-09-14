efa79cdacaae58dbe1771d1f7ea96b45f7e53991
Jannatul Nayeem <123932013+nayeem337@users.noreply.github.com>
2024-09-14 15:27:31 +0600

    first commit

---
+++ pages/about.php
@@ -0,0 +1,25 @@
+<?php
+
+include 'includes/header.php';
+
+if ( ! isset($_SESSION['user_id']))
+{
+    header('Location: action.php?page=login');
+}
+
+?>
+
+<section class="py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1 class="text-center text-capitalize text-bg-success"> this is about page!!! </h1>
+                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab dicta dolorem eos ex tempore. Magni odio quae totam veniam veritatis! Eligendi expedita fugit incidunt maxime molestiae nam repudiandae sunt vero? </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include 'includes/footer.php'; ?>
+
+
---
+++ pages/includes/header.php
@@ -0,0 +1,36 @@
+<?php session_start(); ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/all.css">
+</head>
+<body>
+
+<?php if (isset($_SESSION['user_id'])) { ?>
+
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <div class="container">
+        <a href="" class="navbar-brand"> Logo </a>
+        <ul class="navbar-nav">
+            <li><a href="action.php?page=home" class="nav-link"> Home </a></li>
+            <li><a href="action.php?page=about" class="nav-link"> About </a></li>
+            <li><a href="action.php?page=contact" class="nav-link"> Contact </a></li>
+            <li class="dropdown">
+                <a href="" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"> <?php echo $_SESSION['user_name']; ?> </a>
+                <ul class="dropdown-menu dropdown-menu-dark">
+                    <li><a href="" class="dropdown-item"> Profile </a></li>
+                    <li><a href="action.php?page=logout" class="dropdown-item"> Logout </a></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<?php } ?>
